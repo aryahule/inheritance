0091d28a0f7e4516af1ff4749d43f2927e6c5510
aryahule <aryahule27@gmail.com>
2024-10-13 17:59:10 +0530

    Create single inheritance.cpp

---
+++ single inheritance.cpp
@@ -0,0 +1,27 @@
+Name:Arya Hule
+prn:23070123029
+
+#include <iostream>
+using namespace std;
+
+class Uni {
+public:
+    string uni = "Symbiosis";
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept = "Electronics & Communication";
+};
+
+int main() {
+    Dep department;
+    cout << department.uni << endl;
+    department.discipline();
+    cout << department.dept << endl;
+
+    return 0;
+}
